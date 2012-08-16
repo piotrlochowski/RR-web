7ff4e20bd6ca367d48d7f7695665fbd773cae17f
Piotr Ziemniak <piotr@prosoft.(none)>
2012-08-16 22:40:43 +0200

    RR-gui initial commit

---
+++ racerecordsite/racerecordweb/api.py
@@ -0,0 +1,95 @@
+from django.contrib.auth.models import User
+from tastypie import fields
+from tastypie.resources import ModelResource, ALL,ALL_WITH_RELATIONS
+from racerecordweb.models import Lap, TrialResult, Trial, Location
+from tastypie.authorization import Authorization
+import copy
+
+class LocationResource(ModelResource):
+    class Meta:
+        queryset = Location.objects.all();
+        resource_name = 'location'
+        include_resource_uri = False
+        
+    def alter_list_data_to_serialize(self, request, data_dict):
+        if isinstance(data_dict, dict):
+            if 'meta' in data_dict:
+                # Get rid of the "meta".
+                del(data_dict['meta'])
+                # Rename the objects.
+                data_dict['locations'] = copy.copy(data_dict['objects'])
+                del(data_dict['objects'])
+        return data_dict
+
+
+class TrialResource(ModelResource):
+    location = fields.ForeignKey(LocationResource, 'location')
+
+    class Meta:
+        queryset = Trial.objects.all()
+        resource_name = 'trial'
+        include_resource_uri = False
+        filtering = {
+            'name': ALL,
+        }
+    def alter_list_data_to_serialize(self, request, data_dict):
+        if isinstance(data_dict, dict):
+            if 'meta' in data_dict:
+                # Get rid of the "meta".
+                del(data_dict['meta'])
+                # Rename the objects.
+                data_dict['trials'] = copy.copy(data_dict['objects'])
+                del(data_dict['objects'])
+        return data_dict
+
+class TrialResultResource(ModelResource):
+    #laps = fields.ToManyField(LapResource, 'laps',  full=True)
+
+    class Meta:
+        queryset = TrialResult.objects.all()
+        resource_name = 'trial_result'
+        excludes = ['id']
+        include_resource_uri = True
+        filtering = {
+            'startnumber': ALL,
+        }
+        
+    def alter_list_data_to_serialize(self, request, data_dict):
+        if isinstance(data_dict, dict):
+            if 'meta' in data_dict:
+                # Get rid of the "meta".
+                del(data_dict['meta'])
+                # Rename the objects.
+                data_dict['trial_results'] = copy.copy(data_dict['objects'])
+                del(data_dict['objects'])
+        return data_dict
+
+class LapResource(ModelResource):
+    trial_result = fields.ForeignKey(TrialResultResource, 'trial_result')
+        
+    class Meta:
+        queryset = Lap.objects.all()
+        resource_name = 'lap'
+        excludes = ['id']
+        include_resource_uri = True
+        authorization= Authorization()
+        filtering = {
+            'trial_result': ALL_WITH_RELATIONS,
+        }
+        
+
+class LocationResource(ModelResource):
+    class Meta:
+        queryset = Location.objects.all();
+        resource_name = 'location'
+        include_resource_uri = False
+        
+    def alter_list_data_to_serialize(self, request, data_dict):
+        if isinstance(data_dict, dict):
+            if 'meta' in data_dict:
+                # Get rid of the "meta".
+                del(data_dict['meta'])
+                # Rename the objects.
+                data_dict['locations'] = copy.copy(data_dict['objects'])
+                del(data_dict['objects'])
+        return data_dict
